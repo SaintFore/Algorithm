7afe28c0c7532617ccda50d4865c548b5c503943
Merlin <forevergoodforest@outlook.com>
2025-03-19 09:16:49 +0800

    p1_2

--- Chapter1/p1_1.c
+++ Chapter1/p1_1.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 
 int main(){
-    printf("%d\n", 1+2);
+    printf("%d\n", 1+2);  //3
+    printf("%d\n", 3-4);  //-1
+    printf("%d\n", 5*6);  //30
+    printf("%d\n", 8/4);  //2
+    printf("%d\n", 8/5);  //1 这里是因为输入的是整数，所以小数位被舍弃
     return 0;
 }
